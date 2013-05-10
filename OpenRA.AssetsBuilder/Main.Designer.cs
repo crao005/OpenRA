166d0226235ad9fcfe8bec229dcb75c4f3c324be
Randolph Liu <liuchang0837@163.com>
2013-05-10 14:49:34 +1200

    add the asset builder GUI Framework

---
+++ OpenRA.AssetsBuilder/Main.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace OpenRA.AssetsBuilder
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(236, 65);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(37, 65);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 84);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(840, 595);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Main";
+            this.Text = "Main";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
---
+++ OpenRA.AssetsBuilder/YamlHelper.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenRA.FileFormats;
+
+namespace OpenRA.AssetsBuilder
+{
+    public class YamlHelper
+    {
+        public SoundSettings Sound = new SoundSettings();
+        public Dictionary<string, object> Sections;
+        public Manifest Manifest;
+        public YamlHelper()
+        {
+            Manifest = new Manifest(new String[] { "ra" });
+        }
+
+        public void view()
+        {
+           
+            var aa = Manifest.Sequences;
+           
+            List<MiniYamlNode> y = new List<MiniYamlNode>();
+
+            List<MiniYamlNode> sequences = MiniYaml.FromFile(aa[0]);
+            List<MiniYamlNode> setting = MiniYaml.FromFile(Platform.SupportDir + "settings.yaml");
+
+            foreach (var item in sequences)
+            {
+                Console.WriteLine(item.Key.ToString());
+                var childNode = item.Value;
+                Console.WriteLine(childNode.Nodes.Count);
+            }
+
+            foreach (var item in setting)
+            {
+                Console.WriteLine(item.Key.ToString());
+                var childNode = item.Value;
+                Console.WriteLine(childNode.Nodes.Count);
+            }
+        }
+
+        public List<String> getSequences()
+        {
+            List<String> results =new List<string>();
+            List<MiniYamlNode> sequences = MiniYaml.FromFile(Manifest.Sequences[0]);
+            foreach (var item in sequences)
+            {
+                results.Add(item.Key.ToString());
+                Console.WriteLine(item.Key.ToString());
+                var childNode = item.Value;
+                Console.WriteLine(childNode.Nodes.Count);
+            }
+            return results;
+        }
+
+        public void addSave(String file)
+        {
+            
+            Sections = new Dictionary<string, object>()
+			{
+				{"Sound", Sound},
+			};
+
+            var root = MiniYaml.FromFile(file);
+            foreach (var kv in Sections)
+                root.Add(new MiniYamlNode(kv.Key,FieldSaver.SaveDifferences(kv.Value,Activator.CreateInstance(kv.Value.GetType()))));
+
+            root.WriteToFile(file);
+        }
+    }
+
+    public class SoundSettings
+    {
+        public float SoundVolume = 0.5f;
+        public float MusicVolume = 0.5f;
+        public float VideoVolume = 0.5f;
+        public bool Shuffle = false;
+        public bool Repeat = false;
+        public bool MapMusic = true;
+        public string Engine = "AL";
+
+        //public SoundCashTicks SoundCashTickType = SoundCashTicks.Extreme;
+    }
+}
