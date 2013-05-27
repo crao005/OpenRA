8b0406aacec32d04389f92d19c70ca0632426e12
Geoffrey Fendall <github@therealm.co>
2013-05-27 16:00:11 +1200

    Made repeat on time trigger.

---
+++ OpenRA.Game/MissionScripting/TriggerRepeatOnTime.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpenRA.MissionScripting
+{
+    public class TriggerRepeatOnTime : Trigger
+    {
+        private int time;
+        private int counter;
+
+        // 25 ticks per second.
+        // 1500 per minute
+
+        public TriggerRepeatOnTime(int time)
+        {
+            this.time = time;
+            counter = time;
+        }
+
+        override protected bool ConditionMet()
+        {
+            counter--;
+
+            if (counter <= 0)
+            {
+                counter = time;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
