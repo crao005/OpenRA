b45e8b0bf93cbc6d2c408956f97195f86b0c7c54
Randolph Liu <liuchang0837@163.com>
2013-05-27 16:34:49 +1200

    add the kill unit part to load yaml File --- with Geoffrey

--- OpenRA.Game/MissionScripting/Trigger.cs
+++ OpenRA.Game/MissionScripting/Trigger.cs
@@ -181,6 +181,10 @@ namespace OpenRA.MissionScripting
             {
                 actions.Add(new ActionSpawnUnit(world, child["SpawnUnit"].Value));
             }
+            if (child.ContainsKey("Kill"))
+            {
+                actions.Add(new ActionKillUnit(world, child["Kill"].Value));
+            }
             return actions;
         }
 
